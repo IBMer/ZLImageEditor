8e71d86fdd294d6ed4e1317f029012445a0894cb
Vincent WANG <vincent.x.h.wang@hotmail.com>
2025-11-18 16:23:57 +0800

    add initial ZLImageEditorSwiftUI project

---
+++ ZLImageEditorSwiftUI/ZLImageEditorSwiftUI/ZLImageEditorSwiftUI/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ZLImageEditorSwiftUI
+//
+//  Created by Vincent WANG on 2025/11/18.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
